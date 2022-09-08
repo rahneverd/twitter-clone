7b6c207189eb5ee60e155c1a2493c74c8d0aed9e
rahneverd <rahneverd@gmail.com>
2022-09-08 01:07:38 -0700

    building registration logic

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -6,8 +6,21 @@ router.get('/', middleware.notLoggedIn, (req, res) => {
 });
 
 router.post('/', middleware.notLoggedIn, (req, res) => {
-	console.log(req.body);
-	res.render('register');
+	let firstName = req.body.firstName.trim();
+	let lastName = req.body.lastName.trim();
+	let username = req.body.username.trim();
+	let email = req.body.email.trim();
+	let password = req.body.password;
+
+	let payload = req.body;
+
+	if (firstName && lastName && username && email && password) {
+		console.log('congrats');
+		res.render('register');
+	} else {
+		payload.errorMessage = 'Make sure each field has a valid value.';
+		res.render('register', payload);
+	}
 });
 
 module.exports = router;
