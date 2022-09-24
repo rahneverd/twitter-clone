e66f37da18baa257e8fc87822d7ec503606c0be1
rahneverd <rahneverd@gmail.com>
2022-09-24 07:46:13 -0700

    building the main output layout

--- public/js/common.js
+++ public/js/common.js
@@ -22,5 +22,25 @@ $('#submitTweetButton').click((event) => {
 });
 
 function createTweetLayout(tweet) {
-	return tweet.content;
+	return `<div class='tweet'>
+		<div class='mainContentContainer'>
+			<div class='userImageContainer'>
+				<img src='${tweet.author.profilePic}'>
+			</div>
+			<div class='tweetContentContainer'>
+				<div class='header'>
+					<a href='/profile/${tweet.author.username}'>${tweet.author.firstName} ${tweet.author.lastName}</a>
+					<span class='username'>${tweet.author.username}</span>
+					<span class='date'>${tweet.createdAt}</span>
+				</div>
+				<div class='tweetBody'>
+					<span>${tweet.content}</span>
+				</div>
+				<div class='footer'>
+					
+				</div>
+			</div>
+		</div>
+	
+	</div>`;
 }
